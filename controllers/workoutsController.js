3b56e00b5f9768f148fde8d891820cfff18f9e3c
Jeana Rose <jr.mathis608@gmail.com>
2021-02-06 16:53:19 -0500

    Created file path to exercise page

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -1,6 +1,8 @@
 var express = require("express");
-
 var router = express.Router();
+const path = require("path");
+const PUBLIC_DIR = path.resolve("./", "public");
+// const publicPath = path.join(PUBLIC_DIR, "exercise.html");
 
 // Import the model (cat.js) to use its database functions.
 var Workout = require("../models/Workout");
@@ -12,25 +14,34 @@ router.get("/api/workouts", function (req, res) {
   });
 });
 
+// router.get("/exercise?id=:id", (req, res) => {
+//   Workout.findById({id: req.params.id}).then((foundWorkout) => {
+//     res.json(foundWorkout);
+//   });
+// });
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(PUBLIC_DIR + "/exercise.html"));
+});
+
 router.post("/api/workouts", function (req, res) {
   Workout.create(req.body).then((newWorkout) => {
     res.json(newWorkout);
   });
 });
 
-// TODO: ADD PUT AND DELETE ROUTES
-// NOT SURE IF THESE ARE CORRECT
-router.put("api/workouts/:id", (req, res) => {
-  Workout.findByIdAndUpdate({_id: req.params.id}).then((foundWorkout) => {
-    res.json(foundWorkout);
-  });
-});
+// // TODO: ADD PUT AND DELETE ROUTES
+// // NOT SURE IF THESE ARE CORRECT
+// router.put("api/workouts/:id", (req, res) => {
+//   Workout.findByIdAndUpdate({ _id: req.params.id }).then((foundWorkout) => {
+//     res.json(foundWorkout);
+//   });
+// });
 
-router.put("api/workouts/:id", (req, res) => {
-  Workout.findByIdAndDelete({_id: req.params.id}).then((foundWorkout) => {
-    res.json(foundWorkout);
-  });
-});
-
+// router.put("api/workouts/:id", (req, res) => {
+//   Workout.findByIdAndDelete({ _id: req.params.id }).then((foundWorkout) => {
+//     res.json(foundWorkout);
+//   });
+// });
 
 module.exports = router;
