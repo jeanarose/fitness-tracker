4cb8437ab8b1f04765d9ad515b3316a48b8c45a4
Jeana Rose <jr.mathis608@gmail.com>
2021-02-07 10:43:02 -0500

    Sucessfully built PUT route

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -2,23 +2,8 @@ var express = require("express");
 var router = express.Router();
 const path = require("path");
 const PUBLIC_DIR = path.resolve("./", "public");
-// const publicPath = path.join(PUBLIC_DIR, "exercise.html");
-
-// Import the model (cat.js) to use its database functions.
 var Workout = require("../models/Workout");
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/api/workouts", function (req, res) {
-  Workout.find().then((allWorkouts) => {
-    res.json(allWorkouts);
-  });
-});
-
-// router.get("/exercise?id=:id", (req, res) => {
-//   Workout.findById({id: req.params.id}).then((foundWorkout) => {
-//     res.json(foundWorkout);
-//   });
-// });
 
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(PUBLIC_DIR + "/exercise.html"));
@@ -28,16 +13,24 @@ router.get("/stats", (req, res) => {
   res.sendFile(path.join(PUBLIC_DIR + "/stats.html"));
 });
 
+router.get("/api/workouts", function (req, res) {
+  Workout.find().then((allWorkouts) => {
+    res.json(allWorkouts);
+  });
+});
+
+// Needs to be changed.
 router.post("/api/workouts", function (req, res) {
   Workout.create(req.body).then((newWorkout) => {
     res.json(newWorkout);
   });
 });
 
-// // TODO: ADD PUT AND DELETE ROUTES
+// db.students.update({name: "Steve"}, {$push: {"hobbies":"Extreme Basketweaving"}})
+// Needs to be changed.
 router.put("/api/workouts/:id", (req, res) => {
   const id = req.params.id;
-  Workout.findByIdAndUpdate(id, req.body).then((updatedWorkout) => {
+  Workout.findByIdAndUpdate(id, { $push: {exercises: req.body} }).then((updatedWorkout) => {
     res.json(updatedWorkout);
   });
 });
